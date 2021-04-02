29b7f7c341ce30ecc2a6c4b420c2195927d8aa9e
Sergio Uribe <67348272+sergio9610@users.noreply.github.com>
2021-04-01 21:11:20 -0500

    Se inicia el home page con cursos presenciales

---
+++ php/home.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Cursos Cunati</title>
+	<link rel="stylesheet" href="">
+	<!-- Bootstrap 4 CDN -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<!-- Css Style -->    
+    <link rel="stylesheet" href="../css/style_home.css">
+</head>
+<body>
+	<!-- Logo y Titulo Principal -->
+	<div class="container">
+           <nav class="menuPrincipal navbar fixed-top navbar-expand-md">
+                <a class="tituloPrincipal navbar-brand"><img src="../img/logo.png" alt="Logo" class="logo"></a>
+                <div id="menu" class="collapse navbar-collapse">
+                    <ul class="navbar-nav mr-auto">
+						<li><h1 class="mensajeBienvenida display-3 text-uppercase font-weight-bold">Cursos Cunati</h1></li>
+                    </ul>
+                </div>
+            </nav>
+        </div><br><br>
+
+	<!-- Texto Introductorio -->
+	<div class="textoIntroductorio">
+		<p class="texto_1 mt-3">A continuación encontraras cursos formativos en diferentes areás del conocimiento, algunos con diferentes niveles.<br>
+			Los cursos están ofertados tanto en modalidad presencial como virtual.</p>
+	</div><br>
+
+	<!-- Titulo 1 -->
+	<h1 class="titulo_1">Cursos Presenciales</h1>
+
+	<table class="presenciales">
+		<tr>
+			<th><button class="btn rounded">
+						<a class="btn_ingles_1 text-uppercase font-weight-bold rounded" href="registro.php"> Inglés 1 </a>
+				</button></th>
+			<th><button class="btn rounded">
+						<a class="btn_ingles_2 text-uppercase font-weight-bold rounded" href="registro.php"> Inglés 2 </a>
+				</button></th>
+			<th><button class="btn rounded">
+						<a class="btn_ingles_3 text-uppercase font-weight-bold rounded" href="registro.php"> Inglés 3 </a>
+				</button></th>
+			<th><button class="btn rounded">
+						<a class="btn_frances_1 text-uppercase font-weight-bold rounded" href="registro.php"> Francés 1 </a>
+				</button></th>
+		</tr>
+
+		<tr>
+			<th><button class="btn rounded">
+						<a class="btn_frances_avc text-uppercase font-weight-bold rounded" href="registro.php"> Francés Avanzado </a>
+				</button></th>
+			<th><button class="btn rounded">
+						<a class="btn_aleman_bas text-uppercase font-weight-bold rounded" href="registro.php"> Alemán Básico </a>
+				</button></th>
+			<th><button class="btn rounded">
+						<a class="btn_cocina_1 text-uppercase font-weight-bold rounded" href="registro.php"> Cocina 1 </a>
+				</button></th>
+			<th><button class="btn rounded">
+						<a class="btn_cocina_itl text-uppercase font-weight-bold rounded" href="registro.php"> Cocina Italiana </a>
+				</button></th>
+		</tr>
+
+		<tr>
+			<th><button class="btn rounded">
+						<a class="btn_arte_1 text-uppercase font-weight-bold rounded" href="registro.php"> Arte 1 </a>
+				</button></th>
+			<th><button class="btn rounded">
+						<a class="btn_arte_2 text-uppercase font-weight-bold rounded" href="registro.php"> Arte 2 </a>
+				</button></th>
+			<th><button class="btn rounded">
+						<a class="btn_dibujo text-uppercase font-weight-bold rounded" href="registro.php"> Dibujo </a>
+				</button></th>
+			<th><button class="btn rounded">
+						<a class="btn_carpinteria text-uppercase font-weight-bold rounded" href="registro.php"> Carpinteria </a>
+				</button></th>
+		</tr>
+
+		<tr>
+			<th><button class="btn rounded">
+						<a class="btn_macrame text-uppercase font-weight-bold rounded" href="registro.php"> Macrame </a>
+				</button></th>
+		</tr>
+	</table>
+
+</body>
+</html>
